2eca6841abead341da73964a4110920fd3d5ba29
Subtain96 <awaisiofficial0096@gmail.com>
2026-02-03 23:12:47 +0500

    navbar changing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,21 @@ const Navbar = () => {
   return (
     <div className='navbar'>
 
-        <img src={assets.logo} alt="" />
-      
+       <img src={assets.logo} alt="" className="navbar-logo" />
+       <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile-App</li>
+        <li>Contact Us</li>
+       </ul>
+       <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className='dot'></div>
+        </div>
+        <button>Sign In</button>
+       </div>
     </div>
   )
 }
