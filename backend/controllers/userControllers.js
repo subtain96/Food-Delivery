0177ffea4cb6c71f0f82f981a87eee8024ae812e
Subtain96 <awaisiofficial0096@gmail.com>
2026-03-15 00:40:36 +0500

    feat: implement user registration with validation and token generation

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -9,9 +9,56 @@ import validator from "validator";
 const loginUser = async (req,res) =>{
 }
 
+const createToken = (id) = {
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+
 //Register User
 
 const registerUser = async (req,res) =>{
+
+    const {name , password , email } = req.body;
+
+    try {
+        const exist = await userModel.findOne({email});
+        if (exists ){
+            return res.json({success: false, message: "User already exists"})
+        }
+
+        if (!validator.isEmail(email)) {
+
+           return res.json({success: false, message: "Email is not valid"})
+            
+        }
+        if (password.length<8) {
+
+           return res.json({success: false, message: "Please enter strong password"})
+            
+        }
+
+        //Hashing Password
+
+        const salt = await bcrypt.genSalt(15)
+
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password: hashedPassword,
+        })
+
+        const user = await newUser.save();
+
+        const token = createToken(user._id)
+        res.json({success: true , token})
+
+    } catch (error) {
+        console.log(error);
+         res.json({success: false , message: "Error"})
+        
+    }
 }
 
 export{
